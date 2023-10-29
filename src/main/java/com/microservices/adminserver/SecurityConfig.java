313448d205adf92493801953657d93cdc465a82f
Nitin Chaurasia <gs.nitin@gmail.com>
2023-10-29 11:37:13 -0600

    upgrade

--- src/main/java/com/microservices/adminserver/SecurityConfig.java
+++ src/main/java/com/microservices/adminserver/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.microservices.adminserver;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
  * Created by nichaurasia on Friday, January/24/2020 at 4:38 PM
@@ -13,16 +18,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
-        http.logout().logoutUrl("/logout");
-        http.csrf().disable();
+@EnableWebSecurity
+public class SecurityConfig {
 
-        http.authorizeRequests().antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**").permitAll();
-        http.authorizeRequests().antMatchers("/**").authenticated();
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/ignore1", "/ignore2");
+    }
 
-        http.httpBasic();
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((login) -> login
+                        .loginPage("/login.html")
+                        .loginProcessingUrl("/login")
+                )
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")
+                )
+                .csrf((csrf) -> csrf.ignoringRequestMatchers("/no-csrf"))
+                .httpBasic(withDefaults());
+
+        return http.build();
     }
 }
